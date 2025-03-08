e1f24f21090ff1ab3b4f28adc4908e63951db761
ddjaerospace <ddejong@kent.edu>
2025-03-07 20:33:44 -0500

    Added mode 5- Matlab variable to TI

--- matconv.m
+++ matconv.m
@@ -1,9 +1,9 @@
 % ti nspire matrix to pearson matrix
 % or vice versa
-convdir = input("ti -> pear (1), pear -> ti (2), pear copy to ti, (3) or ti -> matlab? (4) ");
-matrix = input ("matrix text", "s");
-matrix = string(matrix);
+convdir = input("ti -> pear (1), pear -> ti (2), pear copy to ti (3), ti -> matlab (4), or matlab -> ti? (5) ");
 if convdir == 1
+    matrix = input ("matrix text", "s");
+    matrix = string(matrix);
     matrix = append("@MATX",matrix);
     matrix = replace(matrix,"][","};{");
     matrix = replace(matrix,"[","{");
@@ -15,6 +15,8 @@ if convdir == 1
     matrix = replace(matrix,")/(",";");
     disp(matrix)
 elseif convdir == 2
+    matrix = input ("matrix text", "s");
+    matrix = string(matrix);
     matrix = replace(matrix,"-@DIV{","@DIV{-");
     mar = split(matrix,"@DIV{");
     msz = size(mar,1)
@@ -45,6 +47,8 @@ elseif convdir == 2
     matrix = replace(matrix,";",",");
     disp(matrix)
 elseif convdir == 3
+    matrix = input ("matrix text", "s");
+    matrix = string(matrix);
     matrix = replace(matrix,"left bracket","");
     matrix = replace(matrix,"right bracket","");
     matrix = strip(matrix);
@@ -76,10 +80,33 @@ elseif convdir == 3
     matrix = nmat;
     disp(matrix)
 elseif convdir == 4
+    matrix = input ("matrix text", "s");
+    matrix = string(matrix);
     matrix = replace(matrix,"][",";");
     matrix = replace(matrix,"]]","]");
     matrix = replace(matrix,"[[","[");
     eval(matrix)
+elseif convdir == 5
+    matrix = input ("matrix variable");
+    matrix = string(matrix);
+    tmat = matrix';
+    mszf = size(matrix,1)*size(matrix,2)
+    i=0;
+    nmat="[[";
+    while i<mszf
+        i=i+1;
+        nmat = nmat+tmat(i);
+        if i ~= mszf
+            if round(i/size(matrix,2)) == i/size(matrix,2)
+                nmat = nmat + "][";
+            else
+                nmat =nmat + ",";
+            end
+        end
+    end
+    nmat = nmat + "]]";
+    matrix = nmat;
+    disp(matrix)
 else
-    disp("i said 1, 2, 3, or 4 dummy")
+    disp("i said 1, 2, 3, 4, or 5 dummy")
 end
